37770e6f929e7333861d889e449ffff2ee27f693
caochitam <caochitam@gmail.com>
2018-12-07 16:41:01 +0700

    create login and main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: 'benh-nhan', pathMatch: 'full' },
+  // {
+  //   path: 'benh-nhan',
+  //   component: CustomerComponent,
+  //   canActivate: [AuthGuard]
+  // },
+  // {
+  //   path: 'login', 
+  //   component: LoginComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
